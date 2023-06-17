28ff95015e1c955f9cc1567bfe85798ccb5b278d
Moroka <mmopanemoroka@gmail.com>
2023-06-17 19:56:05 +0100

    date picker, cards and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Home from './component/Home/Home';
-import Header from './component/layout/Header/Header';
+import "./App.css";
+import Home from "./component/Home/Home";
+import Header from "./component/layout/Header/Header";
+import Footer from "../src/component/layout/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    <Home/>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import Banner from '../Banner/Banner'
+import React from "react";
+import Banner from "../Banner/Banner";
+import Card from "../Cards/Card";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Banner/>
-    </div>
-  )
-}
+    <div className="home">
+      <Banner />
+      <div className="home_section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+          title="Online Experiences"
+          description="Unique activities we can do together, led by a world of hosts."
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+          title="Unique stays"
+          description="Spaces that are more than just a place to sleep."
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comfortable private places, with room for friends or family."
+        />
+      </div>
+      <div className="home_section">
+        <Card
+          src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+          title="3 Bedroom Flat in Bournemouth"
+          description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+          price="R130/night"
+        />
+        <Card
+          src="https://media.nomadicmatt.com/2019/airbnb_breakup4.jpg"
+          title="Penthouse in London"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="R350/night"
+        />
+        <Card
+          src="https://media.nomadicmatt.com/2018/apartment.jpg"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="R704/night"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/miso/Hosting-519498133786095007/original/ef440c34-d93e-4c3e-b533-492891e3efeb.jpeg?im_w=720"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="R270/night"
+        />
+      </div> 
 
-export default Home
+    </div>
+  );
+};
+
+export default Home;
