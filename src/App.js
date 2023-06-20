5edb75e8fc2c15eaf3a34150b9ac9c1521ea27be
Moroka <mmopanemoroka@gmail.com>
2023-06-20 14:07:18 +0100

    router, searchpage and searchresults

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./component/Home/Home";
 import Header from "./component/layout/Header/Header";
 import Footer from "../src/component/layout/Footer/Footer";
+import SearchPage from "./component/SearchPage/SearchPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
